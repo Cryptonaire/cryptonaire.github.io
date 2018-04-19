870afee2a253d3259cc0a0d27b35731a1664098c
Cryptonaire <38316523+Cryptonaire@users.noreply.github.com>
2018-04-19 19:04:48 +0100

    Add files via upload

--- PlatinumX/script.js
+++ PlatinumX/script.js
@@ -1,11 +1,6 @@
 $(document).ready(function($) {
   $('.listItem').find('#toggleListExpand').click(function(){
-
-    //Expand or collapse this panel
     $(this).next().slideToggle('fast');
-
-    //Hide the other panels
     $(".innerList").not($(this).next()).slideUp('fast');
-
   });
 });
